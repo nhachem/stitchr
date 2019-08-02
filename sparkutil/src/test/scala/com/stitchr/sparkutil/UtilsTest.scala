899d9ca98f702f3ee91fb2cebe4eccaa538b5f9b
nhachem <nabilihachem@gmail.com>
2019-08-02 09:01:59 -0400

    initialize testing framework

---
+++ sparkutil/src/test/scala/com/stitchr/sparkutil/UtilsTest.scala
@@ -0,0 +1,14 @@
+package com.stitchr.sparkutil
+
+import org.scalatest.FunSpec
+
+class UtilsTest extends FunSpec {
+  //TODO time, getRunTime, annotateRuntime
+
+  describe("UtilsTests") {
+    describe("selfChecpoint") {
+      it("TODO: need to add self checkpoint pwersistence...") {}
+    //
+    }
+  }
+}
---
+++ util/src/test/scala/com/stitchr/util/UtilTest.scala
@@ -0,0 +1,22 @@
+package com.stitchr.util
+
+import org.scalatest.FunSpec
+
+class UtilTest extends FunSpec {
+  //TODO time, getRunTime, annotateRuntime
+
+  describe("UtilTest") {
+    describe("time") {
+      it("TODO: should pass through and log the time...") {}
+    //
+    }
+    describe("getRunTime") {
+      it("TODO: get run time") {}
+      //
+    }
+  }
+  describe("annotateRunTime") {
+    it("TODO: annotate run time") {}
+    //
+  }
+}
