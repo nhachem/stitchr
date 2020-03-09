21c31693943dbcff84d9930b2563c5b958b7ea5f
Nabil Hachem <nabilihachem@gmail.com>
2020-03-09 06:49:28 -0400

    fix #14

--- core/src/main/scala/com/stitchr/core/api/ExtendedDataframe.scala
+++ core/src/main/scala/com/stitchr/core/api/ExtendedDataframe.scala
@@ -47,6 +47,42 @@ object ExtendedDataframe {
       if (spark.catalog.tableExists(viewName)) logging.log.warn(s"view name $viewName exists, so skipping")
       else dataFrame.createTempView(viewName)
 
+    // assume the pivot columns are key, value
+    // provide the list as a parameter as sometimes we do not need a full pivot. null means all and we use the schema to generate the pivoted columns
+    def pivot(pivotedColumnsList: List[String] = null, fn: String = "max"): DataFrame = {
+      val pivotColumns =
+        if (pivotedColumnsList == null) dataFrame.select("key").distinct.map(r => s"${r(0)}").collect.toList
+        else pivotedColumnsList
+
+      val l = s"'${pivotColumns.mkString("','")}'"
+      df.createOrReplaceTempView("_tmp")
+
+      val q = s"""SELECT * FROM
+                  | (
+                  | SELECT *
+                  | FROM _tmp
+                  | )
+                  | PIVOT (
+                  | $fn(value)
+                  | FOR key in ( $l )
+                  | ) """.stripMargin
+      spark.sql(q)
+    }
+
+    /**
+     * transforms a dataframe with a string json column columnName into a struc
+     * Note: does not handle cleanly columns that are null or have an array of null.
+     * @param columnName Json column to transform
+     * @return transformed dataframe
+     */
+    def cast2Json(columnName: String): DataFrame = {
+      import org.apache.spark.sql.functions._
+      val schema = spark.sqlContext.read.json(dataFrame.select(columnName).as[String]).schema
+      dataFrame
+        .withColumn(s"${columnName}_jsonString", from_json(col(columnName), schema))
+        .drop(columnName)
+        .withColumnRenamed(s"${columnName}_jsonString", columnName)
+    }
   }
 
 }
